0371992cc98282450292a1803380d1b12c9a0f5f
Lexi <arallen3660@gmail.com>
2023-06-30 09:31:25 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+[DllImport("UnityPlayer")]
+static extern int UnityMain(IntPtr hInstance, IntPtr hPrevInstance,
+    [MarshalAs(UnmanagedType.LPWStr)]ref string lpCmdline, int nShowCmd);
+
+var commandArgs = string.Join(' ', args);
+UnityMain(Process.GetCurrentProcess().Handle,IntPtr.Zero,ref commandArgs,0);
